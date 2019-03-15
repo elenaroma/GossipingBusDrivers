da10e9e89a13694876dc562b965213a94e1f87ea
Elena Rodriguez <elrodriguez@merkleinc.com>
2019-03-15 13:30:28 +0100

    Final commit

--- src/main/java/GossipExchanger.java
+++ src/main/java/GossipExchanger.java
@@ -1,20 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GossipExchanger {
 
-    List<Driver> drivers = new ArrayList<>();
+    private List<Driver> drivers = new ArrayList<>();
 
     public String gossipEchange(List<int[]> driversRoutes) {
 
-        driversRoutes.stream().forEach(driverRoute ->
-                drivers.add(new Driver(driversRoutes.indexOf(driverRoute), driverRoute)));
+        initializeDrivers(driversRoutes);
 
         for (int i = 0; i < 480; i++) {
-            if (drivers.get(0).getNextStop(i) == drivers.get(1).getNextStop(i)) {
+            Map<Integer, List<Driver>> driversByStop = getDriversByStop(i);
+            for (List<Driver> driversAtStop : driversByStop.values()) {
+                Set<Integer> gossipsAtStop = getGossipsAtStop(driversAtStop);
+                driversAtStop.stream().forEach(driver -> driver.setGossips(gossipsAtStop));
+            }
+
+            if (drivers.stream().allMatch(driver -> drivers.size() == driver.getGossips().size())) {
                 return String.valueOf(i + 1);
             }
         }
         return "never";
+    }
+
+    private void initializeDrivers(List<int[]> driversRoutes) {
+        driversRoutes.stream().forEach(driverRoute ->
+                drivers.add(new Driver(driversRoutes.indexOf(driverRoute), driverRoute)));
+    }
+
+    private Map<Integer, List<Driver>> getDriversByStop(int i) {
+        Map<Integer, List<Driver>> driversByStop = new HashMap<>();
+        for (Driver driver : drivers) {
+            if (Objects.isNull(driversByStop.get(driver.getNextStop(i)))) {
+                driversByStop.put(driver.getNextStop(i), new ArrayList<>(Arrays.asList(driver)));
+            } else {
+                driversByStop.get(driver.getNextStop(i)).add(driver);
+            }
+        }
+        return driversByStop;
+    }
+
+    private Set<Integer> getGossipsAtStop(List<Driver> driversAtStop) {
+        Set<Integer> gossipsAtStop = new HashSet<>();
+        for (Driver driver : driversAtStop) {
+            gossipsAtStop.addAll(driver.getGossips());
+        }
+        return gossipsAtStop;
     }
 }
--- src/test/java/GossipExchangerTest.java
+++ src/test/java/GossipExchangerTest.java
@@ -11,7 +11,7 @@ class GossipExchangerTest {
 
     @Test
     public void two_drivers_with_one_same_stop_then_one_stop() {
-        List<int[]> driversRoutes = new ArrayList<int[]>();
+        List<int[]> driversRoutes = new ArrayList<>();
         driversRoutes.add(new int[]{1});
         driversRoutes.add(new int[]{1});
 
@@ -31,4 +31,48 @@ class GossipExchangerTest {
         assertEquals("never", result);
     }
 
+    @Test
+    public void two_drivers_with_second_same_stop_then_two_stops() {
+        List<int[]> driversRoutes = new ArrayList<int[]>();
+        driversRoutes.add(new int[]{1, 3});
+        driversRoutes.add(new int[]{2, 3});
+
+        String result = gossipExchanger.gossipEchange(driversRoutes);
+
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void three_drivers_two_with_one_same_stop_then_never() {
+        List<int[]> driversRoutes = new ArrayList<int[]>();
+        driversRoutes.add(new int[]{1});
+        driversRoutes.add(new int[]{1});
+        driversRoutes.add(new int[]{2});
+
+        String result = gossipExchanger.gossipEchange(driversRoutes);
+
+        assertEquals("never", result);
+    }
+
+    @Test
+    public void example1(){
+        List<int[]> driversRoutes = new ArrayList<int[]>();
+        driversRoutes.add(new int[]{3, 1, 2, 3});
+        driversRoutes.add(new int[]{3, 2, 3, 1});
+        driversRoutes.add(new int[]{4, 2, 3, 4, 5});
+
+        String result = gossipExchanger.gossipEchange(driversRoutes);
+
+        assertEquals("5", result);
+    }
+    @Test
+    public void example2(){
+        List<int[]> driversRoutes = new ArrayList<int[]>();
+        driversRoutes.add(new int[]{2, 1, 2});
+        driversRoutes.add(new int[]{5, 2, 8});
+
+        String result = gossipExchanger.gossipEchange(driversRoutes);
+
+        assertEquals("never", result);
+    }
 }
